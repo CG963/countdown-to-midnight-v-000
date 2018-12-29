d1364ddfb90d4de1e0843ccd16755b7a1b1e56cb
Christopher Garcia <christopher.garcia08@gmail.com>
2018-12-29 04:17:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,5 @@
 def countdown(seconds)
+  seconds == seconds
   while seconds > 0
 	  puts '#{seconds} SECOND(S)!'
 	  seconds -= 1 
@@ -7,6 +8,7 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
+	seconds == seconds
 	while seconds > 0
 	  puts '#{seconds} SECOND(S)!'
 	  seconds -= 1
