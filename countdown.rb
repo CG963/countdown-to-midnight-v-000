3d0c77a51251b7f73618769a18a28f953e5517bb
Christopher Garcia <christopher.garcia08@gmail.com>
2018-12-29 04:12:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,8 @@ def countdown_with_sleep(seconds_left)
   
 	while seconds_left > 0
 	  puts '#{seconds_left} SECOND(S)!'
+	  seconds_left -= 1
 	  sleep(1)
-	  seconds_left -= 1 
 	end
 	"HAPPY NEW YEAR!"
 end
